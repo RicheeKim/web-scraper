5d3ef57f7fca7fca1e8e7c375ff76266260b0fee
rkim91 <richeekim@gmail.com>
2018-09-09 13:08:24 -0400

    attr

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,14 @@
 require 'HTTparty'
 require 'Nokogiri'
 
+
+attr_accessor :parse_page
+
 class Scraper
 
   def initialize
     doc = HTTparty.get("https://store.nike.com/us/en_us/pw/mens-basketball-shoes/7puZ8r1Zoi3")
     @parse_page ||= Nokogiri::HTML(doc)
   end
+
 end
