5f251513072576469d3b3aff68146047937cb1ab
rkim91 <richeekim@gmail.com>
2018-09-09 13:07:43 -0400

    initialize method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,7 @@ require 'Nokogiri'
 class Scraper
 
   def initialize
-    doc = HTTparty.get()
+    doc = HTTparty.get("https://store.nike.com/us/en_us/pw/mens-basketball-shoes/7puZ8r1Zoi3")
+    @parse_page ||= Nokogiri::HTML(doc)
   end
 end
