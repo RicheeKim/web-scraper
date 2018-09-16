780e5dfc585a7c6a721b4cfee4a64e942a3fe7da
rkim91 <richeekim@gmail.com>
2018-09-16 11:07:12 -0400

    variables

--- scraper.rb
+++ scraper.rb
@@ -6,4 +6,16 @@ require 'byebug'
 def scraper
   url = "https://blockwork.cc"
   unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  job_listings = parsed_page.css('div.listingCard') #50 jobs
+
+  job_listings.each do |job_listing|
+    job = {
+      title: job_listing.css('span.job-title').text
+    }
+  end
+
+  byebug
 end
+
+scraper
