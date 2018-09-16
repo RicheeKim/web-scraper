242a93a402b2e63722a9e8020f42ea7feb07d513
rkim91 <richeekim@gmail.com>
2018-09-16 11:14:58 -0400

    skateboard

--- scraper.rb
+++ scraper.rb
@@ -9,14 +9,18 @@ def scraper
   parsed_page = Nokogiri::HTML(unparsed_page)
   job_listings = parsed_page.css('div.listingCard') #50 jobs
 
+  jobs = []
+
   job_listings.each do |job_listing|
     job = {
       title: job_listing.css('span.job-title').text,
       company: job_listing.css('span.company').text,
-      location: job_listing.css('span.location').text
+      location: job_listing.css('span.location').text,
+      url: "https://blockwork.cc" + job_listing.css('a')[0].attributes["href"].value
     }
-  end
 
+    jobs << job
+  end
   byebug
 end
 
