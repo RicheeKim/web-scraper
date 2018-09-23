81878f5d04525507eaf98e6ed8aa38266b133fcc
rkim91 <richeekim@gmail.com>
2018-09-23 08:55:56 -0400

    page

--- scraper.rb
+++ scraper.rb
@@ -8,6 +8,7 @@ def scraper
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
   job_listings = parsed_page.css('div.listingCard') #50 jobs
+  per_page = 50
 
   jobs = []
 
